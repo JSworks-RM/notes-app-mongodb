3de81406be7ef51a65615f4cb5984fab4b15acc1
Nacho RM <joseignaciorm13@gmail.com>
2020-02-28 19:15:49 +0100

    Configuro motor de plantillas con express-handlbars

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
@@ -9,16 +10,23 @@ const app = express()
 // Settings
 app.set( 'port', process.env.PORT || 4000 )
 app.set( 'views', path.join(__dirname, 'views') )
+app.engine( '.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join( app.get('views'), 'layouts' ),
+    partialsDir: path.join( app.get('views'), 'partials' ),
+    extname: '.hbs'
+}) )
+app.set('view engine', '.hbs')
 
 // Middlewares
-app.set( express.urlencoded({ extended : false }) )
+app.use( express.urlencoded({ extended : false }) )
 
 // Global variables
 
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello desde node')
+    res.render('index')
 })
 
 // Static files
